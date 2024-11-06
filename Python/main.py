6f0d631ca21ee3addc7f626fba34183db32b5e16
Fiona Tang <f.tang@iesohealth.com>
2024-11-06 16:22:28 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:Python/main.py

--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,19 @@
 import pytest
 
 def to_roman(number):
-    return "I"
+    number_to_roman = {
+        1: 'I',
+        5: 'V',
+    }
+    return number_to_roman[number]
 
 def test_numeral_one_produces_I():
     assert to_roman(1) == "I"
 
-if __name__ == "main":
+
+def test_numeral_five_produces_V():
+    assert to_roman(5) == "V"
+
+
+if __name__ == "__main__":
     pytest.main()

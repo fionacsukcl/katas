19c4d69c59d9dbf454734cbcd5c566aef5b31f9e
Will Brown <104088223+wbrownieso@users.noreply.github.com>
2024-11-06 16:31:39 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:Python/main.py

--- Python/main.py
+++ Python/main.py
@@ -1,11 +1,17 @@
 import pytest
 
+
 def to_roman(number):
     number_to_roman = {
         1: 'I',
         5: 'V',
+        10: 'X'
     }
+    if number <= 3:
+        return number_to_roman[1] * number
+    
     return number_to_roman[number]
+
 
 def test_numeral_one_produces_I():
     assert to_roman(1) == "I"
@@ -13,6 +19,18 @@ def test_numeral_one_produces_I():
 
 def test_numeral_five_produces_V():
     assert to_roman(5) == "V"
+
+
+def test_numeral_ten_produces_X():
+    assert to_roman(10) == "X"
+
+
+def test_numeral_two_produces_II():
+    assert to_roman(2) == "II"
+
+
+def test_numeral_three_produces_III():
+    assert to_roman(3) == "III"
 
 
 if __name__ == "__main__":
